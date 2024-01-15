e9b1d1653a41dcbb06452ecd4056fe0748dcaab6
HogSquad <114633163+HogSquad@users.noreply.github.com>
2024-01-14 17:43:47 -0700

    writers block no jutsu

--- fg-site/lowforward_main/src/Nothing.jsx
+++ fg-site/lowforward_main/src/Nothing.jsx
@@ -1,8 +1,123 @@
+import aCrumbOfFootsies from "../src/assets/videos/aCrumbOfFootsies.mp4";
+
 export default function Nothing() {
     return (
         <div className="articleStyles">
             <h1>Satsui no Hado: The Power of (doing) Nothing</h1>
-            <h3>Coming soon!</h3>
+
+            <p>If you've been following the guides to this point, you should now have AAs, combos and confirms, and a basic understanding of 
+            offensive and defensive fundamentals- all necessary tools to better navigate the neutral game. This article will be focused on 
+            describing what commitment means in the context of the neutral game, how and why low commitment neutral works, and my approach 
+            to it.</p>
+
+            <p>It's very important to note that playing low commitment does not necessarily mean playing passively. You can still position
+            yourself aggressively and actively engage with your opponent, you're simply doing so in a risk-averse way that makes you difficult
+            to challenge.</p>
+
+            <hr className="smallDivider" />
+
+            <h3>Commitment in fighting games</h3>
+
+            <hr className="smallDivider" />
+
+            <p>Any time you take an action in a fighting game, you can think of it as committing to that action, in the sense that you are
+            locked in to it until its recovery frames end. By extension, you are also open to attack for that action's duration.
+            Choosing actions that are lower commitment- such as walking, blocking, or using pokes with fast startup and low recovery- 
+            limits the number of potential openings you give your opponent. It also makes contesting you midscreen more difficult because 
+            you can potentially respond to anything your opponent does, and the implied threat that you could attack at any time can 
+            make more patient opponents hesitant to hold their ground and back away from you.</p>
+
+            <p>On the flipside, not committing to bigger buttons can also empower your opponents to do something risky and press their 
+            big buttons or commit to things like jumps, dashes, etc. Being able to use our anti-airs, punish confirms, parries, etc to 
+            shut down those options when they do happen are absolutely essential to maintain your position and increase pressure 
+            on your opponent. Remember, you're not always playing low commitment to be passive unless it benefits you- typically this is 
+            if you have a life lead and a good screen position since you have no incentive to approach, or you're in burnout and can 
+            afford to block until the duration ends. </p>
+
+            <p>Low commitment neutral works best as a baseline when you can use your basic movement and blocks to get an idea for how 
+            your opponent plays, when and how they like to press buttons, and safely put yourself in a favourable position for when you 
+            want to engage. You cannot always play non-committally, as some matchups and screen positions can force you to constantly make 
+            committal decisions in order to get in- while this exists in SF6 to some extent, especially when closing in on zoners with low 
+            drive, this is much more prevalent in anime fighters like GGST, for example, where dashblock IBs 
+            or alternative mobility tools are all but mandatory at high levels to close in on characters with strong mid and long-range screen
+            control. It's an incredibly strong style to use in a few situations: when you're not sure of the opponent's habits and want to 
+            gather information, when your opponent is constantly going all in, and when your opponent is patient and playing non-committally 
+            themselves.</p>
+
+            <hr className="smallDivider" />
+
+            <h3>Musings on low commitment neutral</h3>
+
+            <hr className="smallDivider" />
+
+            <p>Weaker opponents will generally lose outright to someone who does not constantly present big openings to punish, since they 
+            don't understand how to approach someone who isn't willing to go all in on every interaction. Defeating this kind of player with 
+            a low commitment style does take a lot of practice and can be difficult to do because it's inherently reactive; being able to 
+            consistently check their movement, anti-air them, defending and punishing their all ins, etc, can overload your mental stack and 
+            make you too passive if you aren't ready. It's impossible to always be ready for everything, but you can be looking for one or 
+            two things they have shown they like to do and be ready for those, but have a defensive catch-all against other options. For 
+            example, if your opponent is a Cammy who likes to poke with 2MK drive rushes and varying air mixups with jumps, divekicks, 
+            and hooligans, you can play around the max range of their 2MK and look for anti-airs and openings to poke them, either block or 
+            delay tech when they drive rush 2MK and allow them to burn themselves out,
+            but then use parries as a blanket check to defend against options like DI or spin knuckle. If they start dashing up to you, then 
+            you can adapt by mashing with your 4-frame to check them and hit confirming it. This makes scouting your opponent's habits much more 
+            manageable, since you only need 2 or 3 things to respond to everything they're doing instead of potentially 5 or more. </p>
+
+            <p>Even at higher levels of play, many people have fairly linear attacking and defending flowcharts that you can figure out with 
+            patience- being able to do this is a skill that must be practiced, and consistency comes with a lot of experience and failure. A 
+            lot of it honestly comes down to pattern recognition. If you don't want to risk attacking someone unpredictable, switch up your 
+            tactics from running your regular strike/throw and focus on winning neutral, where you are more comfortable and they have an 
+            exploitable blind spot. If you don't know how to defend against a hyper aggressive attacker, especially one that has a good sense of 
+            when to bait reversals, block it out and be willing to take a few throws until you can make an educated guess as to what they will 
+            choose. One way to think of it is like you're getting X number of guesses to escape, where X is the number of throws they 
+            need to KO you, or you can gamble and lose in 2-3 hits from full health if you're wrong. You won't always win against 
+            volatile players like that even when you're experienced, and that's okay. There's always a takeaway or adaptation that could 
+            have been made, or worst case you have something concrete to lab an answer to if you were getting knowledge checked. Remember 
+            that hyper aggressive playstyles are especially popular in ranked because they are effective at getting quick wins in short sets 
+            and are hard to stop without practice, but this style of patient play will completely stonewall the overwhelming majority of the 
+            playerbase because it is simply a side of the game they rarely have to engage with. Top level players can play either way, and 
+            constantly rotate their options or make very slight adjustments to their moves and timings to make themselves unpredictable. 
+            It's important to remember to not over rely on one specific approach to neutral as you become a more well-rounded player, 
+            especially when you come across strong opponents who do adapt quickly.</p>
+
+            <hr className="smallDivider" />
+
+            <h3>What low commitment neutral looks like</h3>
+
+            <hr className="smallDivider" />
+
+            <p>In the following video example, you can see low commitment neutral in action against a local Kim. I had a lot of respect for 
+            their offense and ability to snowball an advantage, so I took extra care to play slowly and focus on gaining ground and 
+            watching to see what they did when I attacked sparsely. The close-range fireballs were thrown when I was reasonably sure there 
+            would be either pokes to counterpoke or forward movement- if you are in a similar situation and don't have a fireball, you can use a larger 
+            whiff punish button, such as Jamie's 5HK, or attempt to check them with something like a 5MK or 2MK. 
+            The longer range hados were to test willingness to reaction teleport and slide that round. 
+            Since we were playing at my pace, my strings weren't being perfect parried or otherwise punished, and I was winning in neutral, 
+            I was for the most part okay to not risk eating a wakeup level 3 and keep up the pressure midscreen. I did sneak in some 
+            throws near the end once it had been clearly established that we were both respecting each others' wakeup, 
+            just to avoid being too linear. The 2HP was actually straight up a misinput 5HP, whoops.
+            </p>
+
+            <figure>
+            <video className="w-5/6 sm:w-3/4 md:w-4/6" src={aCrumbOfFootsies} type="video/mp4" preload="metadata" controls></video>
+            <figcaption>Example of a round won primarily through low commitment neutral. Pay special attention to how I approach and hold 
+            space.
+            Even though there are a few mistakes and missed punishes, I get enough openings from watching their habits over the course of the 
+            round to comfortably close things out.
+            </figcaption>
+            </figure>
+
+            <hr className="smallDivider" />
+
+            <p>In summary, low commitment neutral is a strong baseline approach for figuring out your opponent's habits, countering 
+            stereotypical online playstyles, and dictating the pace of a match. It takes a lot of practice and a strong understanding 
+            of what your options are on offence and defence to pull off consistently, but it is one of the most reliable ways to beat 
+            opponents of all skill levels. In the next article, we'll break down some of the individual techniques shown in the video as 
+            we deepen our understanding of how to hold our ground and control space.</p>
+
+            <hr className="smallDivider" />
+
         </div>
+        
     )
 }
