2cf26e5cdfe374706210138eab6b715d745369a6
HogSquad <114633163+HogSquad@users.noreply.github.com>
2024-02-10 19:03:21 -0700

    Update Home.jsx
    better fix

--- fg-site/lowforward_main/src/Home.jsx
+++ fg-site/lowforward_main/src/Home.jsx
@@ -3,11 +3,11 @@ import master from '../src/assets/images/master.png'
 export default function Home() {
    return (
    <div className="articleStyles">
-            <img className='w-screen h-600 object-cover object-left' 
+            <img className='w-screen max-w-screen-md h-600 p-12 object-cover object-left' 
             src={master}
             alt='/' />
 
-            <h1>Your resource for learning how to play fighting games at a high level.</h1>
+            <h1 className='p-8'>Your resource for learning how to play fighting games at a high level.</h1>
                 </div>
                 
    )
