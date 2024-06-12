788690f6b18de87a7f7450952c65d86155aa79a8
HogSquad <114633163+HogSquad@users.noreply.github.com>
2024-06-11 23:08:29 -0600

    Update HomeItem.jsx
    More spicing up the home nav components.

--- fg-site/lowforward_main/src/HomeItem.jsx
+++ fg-site/lowforward_main/src/HomeItem.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const HomeItem = ({img, title, page}) => {
     return (
-    <div className='relative flex items-center justify-center h-auto max shadow shadow-emerald-400 rounded-xl group hover:bg-gradient-to-l from-gray-400 to-emerald-500'>
+    <div className='relative flex items-center justify-center h-auto max shadow shadow-emerald-400 rounded-xl group hover:bg-gradient-to-l from-gray-400 to-emerald-500 active:from-emerald-500 active:to-gray-400'>
         <img src={img} alt="LowForward" className='rounded-xl group-hover:opacity-10 max-w-60' />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] object-scale-down'>
             <h3 className='text-2xl font-bold text-whitesmoke tracking-wider text-center'>
                 {title}
             </h3>
             <a href={page}>
-                <p className='text-center p-3 rounded-lg bg-whitesmoke text-gray-700 fond-bold cursor-pointer text-lg'>Click here!</p>
+                <p className='text-center p-3 rounded-lg drop-shadow-lg bg-whitesmoke hover:bg-gradient-to-l active:bg-gradient-to-r active:from-emerald-300 active:to-slate-300 hover:from-emerald-200 hover:to-slate-200 text-gray-600 hover:text-gray-700 active:text-slate-800/95 font-bold cursor-pointer text-lg duration-200'>Click here!</p>
             </a>
         </div>
     </div>
