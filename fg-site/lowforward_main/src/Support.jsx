ab1703e14464653d2b703aeb8f25173a7c34b772
TruffleHogDev <114633163+TruffleHogDev@users.noreply.github.com>
2025-03-10 04:55:05 -0600

    Update Support.jsx
    Rewrote the support blurb

--- fg-site/lowforward_main/src/Support.jsx
+++ fg-site/lowforward_main/src/Support.jsx
@@ -14,16 +14,32 @@ export default function Support() {
         />
         <article className="articleStyles p-3">
           <h2 className="text-2xl font-bold text-center mb-4">
-            Support Our Mission
+            Support My Mission
           </h2>
           <p className="text-wrap mb-4">
-            This website is built with a simple goal: to be a free, accessible
-            community resource for everyone. Learning should never be hidden
-            behind a paywall, and I am committed to keeping this platform open
-            and available to all.
+            This website is built with one simple goal: to be a free, accessible
+            community resource for everyone. When I was new to fighting games, I
+            was extremely frustrated by how hard it was to find information on
+            fighting game strategy, from people who knew what they were talking
+            about. Even extremely talented players couldn't articulate what they
+            were doing in a way that made any sense, and the ones that could
+            would often assume a baseline level of competency that I just didn't
+            have yet. When I was a weak intermediate player just breaking into
+            Celestial for the first time, I couldn't find a single centralized
+            resource for explaining advanced strategies or how to really
+            approach the nuances behind neutral that would take me to the next
+            level. I had to play in several tournaments and lose hundreds of
+            games against professionals that were willing to tell me what I did
+            wrong and play me until I started to do things correctly that I
+            began to make any real progress.
           </p>
-          <p className="text-wrap mb-4">
-            Your support helps me maintain and grow this platform. Whether it's
+
+          <p>
+            I want to bridge that gap in knowledge for all levels of play, and
+            condense that wisdom into my articles here so that you are
+            guaranteed to learn something, whether this is your first fighting
+            game or you've been playing for years and pushing 1600 MR. Your
+            support helps me maintain and grow this platform. Whether it's
             feedback, suggestions, or a small donation, every contribution makes
             a difference.
           </p>
@@ -38,10 +54,6 @@ export default function Support() {
             </Link>
             .
           </h4>
-          <p className="text-center mb-4">
-            Thank you for being part of this community. Together, we can create
-            something truly valuable.
-          </p>
         </article>
       </div>
     </>
